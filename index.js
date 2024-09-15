48a69d207f34ffda3f34e3133d5a3263390d5862
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-09-15 13:00:22 +0530

    created first endpoint & routes

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json");
 
 const app = express();
 const PORT = 8000;
+
+//Routes
+app.get("/users", (req, res) => {
+  const html = `
+          <ul>
+              ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+          </ul>
+      `;
+  res.send(html);
+});
+
+//ENDPOINTS
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
 
 app.listen(PORT, () => console.log("Server is started at port 8000"));
