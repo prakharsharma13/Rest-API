792ac39f656e144deeba3b5d64b0944caa7eae39
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-09-15 22:34:04 +0530

    HTTP status codes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
 const fs = require("fs");
+const { error } = require("console");
 const app = express();
 const PORT = 8000;
 
@@ -35,14 +36,25 @@ app.get("/api/users", (req, res) => {
 app.get("/api/users/:id", (req, res) => {
   const id = Number(req.params.id);
   const user = users.find((user) => user.id === id);
+  if (!user) return res.status(404).json({ error: "user not found" });
   return res.json(user);
 });
 
 app.post("/api/users", (req, res) => {
   const body = req.body;
+  if (
+    !body.first_name ||
+    !body.last_name ||
+    !body.gender ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
+  ) {
+    return res.status(400).json("All fields are required");
+  }
   users.push({ ...body, id: users.length + 1 });
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.json({ status: "Success", id: users.length });
+    return res.status(201).json({ status: "Success", id: users.length });
   });
 });
 
